f6575234019fe591bccf6b2d8ded21a630e36bc6
DS <ischegal@googlemail.com>
2012-05-25 16:44:20 +0200

    Tests zum Spielerverbinden (1PC) eingefuegt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "Spiel.h"
 #include <iostream>
+#include "Spieler.h"
 
 int main()
 {
 	Spiel *Testspiel = new Spiel();
+	Spieler *Testspieler1 = new Spieler();
+	Spieler *Testspieler2 = new Spieler();
+	Spieler *Testspieler3 = new Spieler();
 
-	system("Pause");
+	Testspieler1->verbindezuSpiel(Testspiel);
+	Testspieler2->verbindezuSpiel(Testspiel);
+	Testspieler3->verbindezuSpiel(Testspiel);
+
+	//system("Pause");
 	return 0;
 }
